badb52b75a2dba5258f91ef57f1c3113d6469aa0
vajaboy <v_chelidze2@cu.edu.ge>
2021-09-20 10:40:27 +0400

    finished parser

--- app/Feeds/Vendors/VNT/Parser.php
+++ app/Feeds/Vendors/VNT/Parser.php
@@ -5,42 +5,52 @@ namespace App\Feeds\Vendors\VNT;
 
 
 use App\Feeds\Feed\FeedItem;
+use App\Feeds\Utils\ParserCrawler;
+use App\Helpers\StringHelper;
+use JetBrains\PhpStorm\Pure;
 
 class Parser extends \App\Feeds\Parser\HtmlParser
 {
+    public array $images;
+    public array $attributes;
+    public array $variations;
+
+    // взять json дочерных продуктов с саита
+    public function beforeParse(): void
+    {
+        if($this->getCategories()[0] !== "Bundles") {
+            $json = html_entity_decode($this->getAttr(".variations_form","data-product_variations"));
+
+            $this->variations = json_decode($json,true);
+        } else $this->variations = [];
+    }
+
+    public function getAvail(): ?int {
+        return self::DEFAULT_AVAIL_NUMBER;
+    }
+
+    public function getProductCode(): string
+    {
+        return $this->getVendor()->getSupplierName()."-".$this->getProduct();
+    }
 
     public function getProduct(): string
     {
-        echo $this->getText('.product_title');
-        return $this->getText('.product_title') ?? "default name";
-    }
-
-
-    public function getCostToUs(): float
-    {
-        // todo
-        return 0.5;
+        return $this->getText('.product_title');
     }
 
     public function getMpn(): string
     {
-        return $this->getText(".sku") ?? "";
+        if(!$this->isGroup())
+            return $this->getText(".sku");
+        return "";
     }
 
     public function getImages(): array
     {
-        return [$this->getAttr(".wp-post-image.lazyloaded","src")];
-    }
-
-    public function getAvail(): ?int
-    {
-        return self::DEFAULT_AVAIL_NUMBER;
-    }
-
-    public function getListPrice(): ?float
-    {
-        // todo
-        return 0.5;
+        if(!$this->isGroup())
+            return $this->parseSrcset($this->variations[0]["image"]["srcset"]);
+        return [];
     }
 
     public function getDescription(): string
@@ -48,21 +58,117 @@ class Parser extends \App\Feeds\Parser\HtmlParser
         return $this->getHtml(".woocommerce-product-details__short-description");
     }
 
-    public function getShortDescription(): array
+    public function getCategories(): array
     {
-        // todo
-        return ["feature1"];
+        $categories = [];
+        $this->filter(".posted_in a")->each(function (ParserCrawler $c) use (&$categories) {
+            $categories[] = $c->text();
+        });
+
+        return $categories;
+    }
+
+    public function getCostToUs(): float
+    {
+        if(!$this->isGroup())
+            return 5.5;
+        return 0;
+    }
+
+    public function getListPrice(): ?float
+    {
+        if(!$this->isGroup())
+            return 6.5;
+        return null;
     }
 
     public function isGroup(): bool
     {
-        // todo
-        return false;
+        return true;
     }
 
     public function getChildProducts(FeedItem $parent_fi): array
     {
-        // todo
-        return [];
+        $child = [];
+        $i = 0;
+        if(!$this->isBundle($parent_fi))
+            foreach($this->variations as $variation) {
+            $fi = clone $parent_fi;
+
+            $fi->setProductCode($this->getVendor()->getSupplierName() . "-" . $i);
+            $fi->setMpn($variation["sku"]."-".$i);
+            $fi->setCostToUs(StringHelper::getMoney($variation["display_price"]));
+            $fi->setListPrice(StringHelper::getMoney($variation["display_regular_price"]));
+            $fi->setRAvail($variation["is_in_stock"] ? self::DEFAULT_AVAIL_NUMBER : 0);
+            $fi->setImages($this->parseSrcset($variation["image"]["srcset"]));
+
+            $i++;
+            array_push($child,$fi);
+        }
+        // если bundle
+        else {
+            $i =0;
+            // продукты bundle
+            $this->filter(".woosb-product")->each(function (ParserCrawler $c) use (&$child,$parent_fi,&$i) {
+
+                $productName = $c->getAttr(".woosb-product", "data-name");
+
+                // если такой дочерный продукт уже был парсирован, взять из индексированного масива
+                // если нет, вставить напоминание
+                if (isset($this->getVendor()->feed[$productName])) {
+                    $product = clone $this->getVendor()->feed[$productName];
+                    $product->setMpn($product->getProduct() . "-" . $i);
+                    $product->setProductCode($this->getVendor()->getSupplierName() . "-" . $i);
+                }
+                else {
+                    $product = clone $parent_fi;
+                    $product->setAltNames([$productName]);
+                }
+
+                // ставим скидучную цену
+                $this->getVendor()->setBundleProductPrice($product);
+
+
+                $i++;
+                array_push($child,$product);
+            });
+        }
+
+        return $child;
+    }
+
+    public function getShortDescription(): array
+    {
+        $arr = [];
+        $this->filter(".woocommerce-product-attributes")->each(function(ParserCrawler $c) use (&$arr) {
+            $arr[$c->getText(".woocommerce-product-attributes-item__label")] = $c->getText(".woocommerce-product-attributes-item__value");
+        });
+
+        return $arr != [] ? $arr : ["empty description"];
+    }
+
+    // хелпер методы
+    private function isBundle(FeedItem $fi) {
+        return $fi->getCategories()[0] === "Bundles";
+    }
+
+    private function parseSrcset(string $srcset)
+    {
+        $arr = explode(",", $srcset);
+        foreach($arr as &$str) {
+            $str = explode(" ",trim($str))[0];
+        }
+
+        return $arr;
+    }
+
+    private function parseAttributes(array $variation)
+    {
+        $attributes = [];
+        foreach($variation["attributes"] as $key => $value) {
+            $attributes[$key] = $value;
+        }
+
+        return $attributes;
     }
 }
--- app/Feeds/Vendors/VNT/Vendor.php
+++ app/Feeds/Vendors/VNT/Vendor.php
@@ -6,15 +6,31 @@ namespace App\Feeds\Vendors\VNT;
 
 use App\Feeds\Feed\FeedItem;
 use App\Feeds\Processor\HttpProcessor;
+use App\Feeds\Storage\AbstractFeedStorage;
 use App\Feeds\Utils\Data;
 use App\Feeds\Utils\Link;
+use App\Repositories\DxRepositoryInterface;
 
 class Vendor extends HttpProcessor
 {
+
     protected array $first = [ "https://vanatisanes.com/"];
 
+    protected const CHUNK_SIZE = 10;
+    protected const DELAY_S = 3;
+    protected const REQUEST_TIMEOUT_S = 60;
+    protected const STATIC_USER_AGENT = true;
+
+    // фид с индексом имён для O(1) поиска
+    public array $feed = [];
+
+    protected array $headers = [
+        "Accept" => "*/*",
+        "Host" => "vanatisanes.com"
+    ];
+
     public const CATEGORY_LINK_CSS_SELECTORS = [".menu-item > a"];
-    public const PRODUCT_LINK_CSS_SELECTORS = [".product_type_variable"];
+    public const PRODUCT_LINK_CSS_SELECTORS = [".product-category.product-info"];
 
     public function filterProductLinks(Link $link): bool
     {
@@ -23,6 +39,44 @@ class Vendor extends HttpProcessor
 
     public function isValidFeedItem(FeedItem $fi): bool
     {
-        return $fi->getMpn() !== "";
+        return $fi->getProduct() !== null;
+    }
+
+    // индексируем каждый новый FeedItem в мап feed для O(1) поиска
+    public function afterProcessItem()
+    {
+        $last_key = key(array_slice($this->feed_items, -1, 1, true));
+        if($last_key != "") {
+            $item = $this->feed_items[$last_key];
+            $this->feed[$item->getProduct()] = &$item;
+        }
+    }
+
+    // вставим дочерные продукты которые ешё не вставлени
+    public function afterProcess()
+    {
+        foreach($this->feed_items as $feed_item) {
+            $i = 0;
+            foreach($feed_item->child_products as &$child_product) {
+                if(isset($child_product->alt_names[0])) {
+                    $child_product = clone $this->feed[$child_product->alt_names[0]];
+                    $child_product->setMpn($feed_item->getProduct()."-".$i);
+                    $child_product->setProductCode($feed_item->getProductCode()."-".$i);
+                    $i++;
+
+                    // ставим скидочную цену
+                    $this->setBundleProductPrice($child_product);
+                }
+            }
+        }
+    }
+
+    // хелпер методы
+    public function setBundleProductPrice(&$fi) {
+        foreach($fi->child_products as &$childProduct) {
+            $original_price = $childProduct->getListPrice() ?? 0.0;
+            $childProduct->setCostToUs($original_price - (15/100 * $original_price));
+            echo $childProduct->product." discount price: ".$childProduct->cost_to_us.PHP_EOL;
+        }
     }
 }
